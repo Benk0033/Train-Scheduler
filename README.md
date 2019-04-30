0032395f9600ccf3ad5ae83cfb4d1216cd05e165
Benk0033 <benk0033@gmail.com>
2019-04-29 23:02:22 -0700

    final

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Train-Scheduler
+1. This App provides up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+2. Data is hosted through Firebase
+3. User enters Train name, destination, time train first arrived (In military time), and train arrvial frequency (In minutes)
+4. App will calculate when the next train will arrive and how many minutes away it is
--- assets/js/app.js
+++ assets/js/app.js
@@ -13,14 +13,17 @@ $(document).ready(function () {
 
     var database = firebase.database();
 
+    // click event for adding trains when clicking the submit button
     $("#add-train-btn").on("click", function (event) {
         event.preventDefault();
 
+        // variables to store user inputs
         var trainName = $("#train-name-input").val().trim();
         var trainDestination = $("#destination-input").val().trim();
         var firstTrainTime = moment($("#time-input").val().trim(), "HH:mm").format("HH:mm");
         var trainFrequency = $("#frequency-input").val().trim();
 
+        // Creates local "temporary" object for holding train data
         var newTrain = {
             name: trainName,
             destination: trainDestination,
@@ -28,6 +31,7 @@ $(document).ready(function () {
             frequency: trainFrequency
         };
 
+        // pushes train data to firebase
         database.ref().push(newTrain);
 
         console.log(newTrain.name);
@@ -35,18 +39,21 @@ $(document).ready(function () {
         console.log(newTrain.trainTime);
         console.log(newTrain.frequency);
 
+        // clears input boxes after submission
         $("#train-name-input").val("");
         $("#destination-input").val("");
         $("#time-input").val("");
         $("#frequency-input").val("");
     });
 
+    // event listener that adds train data as a new row into html and new data into firebase when user submits new entry
     database.ref().on("child_added", function (snapshot) {
 
         var sv = snapshot.val();
 
         console.log(sv);
 
+        // strores firebase values into vairables
         var trainName = sv.name;
         var trainDestination = sv.destination;
         var firstTrainTime = sv.trainTime;
@@ -57,33 +64,42 @@ $(document).ready(function () {
         console.log(firstTrainTime);
         console.log(trainFrequency);
 
+        // ensures first train time is always behind current time by subtracting one year
         var firstTrainTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
         console.log(firstTrainTimeConverted);
 
+        // stores current time into a variable
         var currentTime = moment();
 
+        // difference between current time and first train time
         var diffTime = currentTime.diff(firstTrainTimeConverted, "minutes");
         console.log(diffTime);
 
+        // calculates time apart
         var tRemainder = diffTime % trainFrequency;
         console.log(tRemainder);
 
+        // calculates minutes until train arrives
         var minutesAway = trainFrequency - tRemainder;
         console.log(minutesAway);
 
+        // next arrival time
         var nextArrival = currentTime.add(minutesAway, "minutes");
         console.log(nextArrival);
 
+        // creates new row
         var newRow = $("<tr>").append(
-            $("<td>").text(trainName),
-            $("<td>").text(trainDestination),
-            $("<td>").text(trainFrequency),
-            $("<td>").text(nextArrival.format("hh:mm a")),
-            $("<td>").text(minutesAway)
+            $("<td>").html(trainName),
+            $("<td>").html(trainDestination),
+            $("<td>").html(trainFrequency),
+            $("<td>").html(nextArrival.format("hh:mm a")),
+            $("<td>").html(minutesAway)
         );
 
+        // appends new row
         $("#train-table").append(newRow);
 
+        // handle the errors
     }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
